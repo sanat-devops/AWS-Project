896545e76cdc3a8f7f1b35d913f49d30c9a0cb1d
sanat-devops <mishrasanat007@gmail.com>
2025-12-04 23:49:01 +0530

    modified cli file

--- src/cli.py
+++ src/cli.py
@@ -2,40 +2,33 @@ import argparse
 from src.aws import ec2 as ec2_mod
 from src import config
 
-
-
-
 def cmd_ec2_list(args):
-instances = ec2_mod.list_instances(region_name=args.region)
-if not instances:
-print("No instances found")
-return
-for i in instances:
-uptime = ec2_mod.uptime_seconds(i['LaunchTime'])
-hrs = uptime / 3600 if uptime else None
-print(f"{i['InstanceId']}: state={i['State']}, type={i['InstanceType']}, uptime_hrs={hrs:.2f}")
+    instances = ec2_mod.list_instances(region_name=args.region)
 
+    if not instances:
+        print("No instances found")
+        return
 
-
+    for i in instances:
+        uptime = ec2_mod.uptime_seconds(i['LaunchTime'])
+        hrs = uptime / 3600 if uptime else 0
+        print(f"{i['InstanceId']}: state={i['State']}, type={i['InstanceType']}, uptime_hrs={hrs:.2f}")
 
 def main():
-parser = argparse.ArgumentParser(prog='aws-ops-toolkit')
-sub = parser.add_subparsers(dest='cmd')
+    parser = argparse.ArgumentParser(prog='aws-ops-toolkit')
+    sub = parser.add_subparsers(dest='cmd')
 
+    p = sub.add_parser('ec2-list', help='List EC2 instances')
+    p.add_argument('--region', default=config.DEFAULT_REGION)
+    p.set_defaults(func=cmd_ec2_list)
 
-p = sub.add_parser('ec2-list', help='List EC2 instances')
-p.add_argument('--region', default=config.DEFAULT_REGION)
-p.set_defaults(func=cmd_ec2_list)
+    args = parser.parse_args()
 
+    if not hasattr(args, 'func'):
+        parser.print_help()
+        return
 
-args = parser.parse_args()
-if not hasattr(args, 'func'):
-parser.print_help()
-return
-args.func(args)
-
-
-
+    args.func(args)
 
 if __name__ == '__main__':
-main()
+    main()
